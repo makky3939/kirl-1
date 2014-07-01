a8bc4fef9797e35c7d41e004d53bdae1662de123
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2014-07-02 01:22:53 +0900

    viewの調整

--- controller/controller.rb
+++ controller/controller.rb
@@ -1,38 +1,37 @@
-#!/usr/bin/ruby
 # -*- coding: utf-8 -*-
 
 class Query
   def initialize(params)
     @attribute = attribute
 
-    @limit  = params["limit"]
-    @offset = params["offset"]
-    @range  = params["range"]
-    @nbc    = params["nbc"]
+    @limit  = params['limit']
+    @offset = params['offset']
+    @range  = params['range']
+    @nbc    = params['nbc']
 
-    @input_1_text = params["input_1_text"]
-    @input_1_field = params["input_1_field"]
+    @input_1_text = params['input_1_text']
+    @input_1_field = params['input_1_field']
 
-    @input_2_text = params["input_2_text"]
-    @input_2_field = params["input_2_field"]
-    @input_2_operator_symbol = params["input_2_operator_symbol"]
+    @input_2_text = params['input_2_text']
+    @input_2_field = params['input_2_field']
+    @input_2_operator_symbol = params['input_2_operator_symbol']
 
-    @input_3_text = params["input_3_text"]
-    @input_3_field = params["input_3_field"]
-    @input_3_operator_symbol = params["input_3_operator_symbol"]
+    @input_3_text = params['input_3_text']
+    @input_3_field = params['input_3_field']
+    @input_3_operator_symbol = params['input_3_operator_symbol']
 
-    if @input_1_field == "" && @input_2_field == "" && @input_3_field == ""
+    if @input_1_field == '' && @input_2_field == '' && @input_3_field == ''
       @input_type_single = true
     else
       @input_type_single = false
     end
 
-    @input_1_field = "title" if @input_1_field == ""
-    @input_2_field = "title" if @input_2_field == ""
-    @input_3_field = "title" if @input_3_field == ""
+    @input_1_field = 'title' if @input_1_field == ''
+    @input_2_field = 'title' if @input_2_field == ''
+    @input_3_field = 'title' if @input_3_field == ''
 
-    @input_2_operator_symbol = "and" if @input_2_operator_symbol == "" && @input_2_text != ""
-    @input_3_operator_symbol = "and" if @input_3_operator_symbol == "" && @input_3_text != ""
+    @input_2_operator_symbol = 'and' if @input_2_operator_symbol == '' && @input_2_text != ''
+    @input_3_operator_symbol = 'and' if @input_3_operator_symbol == '' && @input_3_text != ''
 
     @where = ''
     if @input_type_single
@@ -117,21 +116,21 @@ class Query
 
   def attribute
     [
-      "book.nbc",
-      "isbn.isbn",
-      "book.title",
-      "book.author",
-      "book.pub",
-      "book.date",
-      "book.phys",
-      "note.note", 
-      "ed.ed",
-      "series.series",
-      "titleheading.titleheading",
-      "authorheading.authorheading",
-      "holdingsrecord.holdingsrecord",
-      "holdingloc.holdingloc",
-      "holdingphys.holdingphys"
+      'book.nbc',
+      'isbn.isbn',
+      'book.title',
+      'book.author',
+      'book.pub',
+      'book.date',
+      'book.phys',
+      'note.note', 
+      'ed.ed',
+      'series.series',
+      'titleheading.titleheading',
+      'authorheading.authorheading',
+      'holdingsrecord.holdingsrecord',
+      'holdingloc.holdingloc',
+      'holdingphys.holdingphys'
     ]
   end
 
