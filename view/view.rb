f28ad388e54a97d2aefaf4ff5ad87463a8c2210b
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2014-07-04 18:37:02 +0900

    metaタグを追加

--- view/view.rb
+++ view/view.rb
@@ -113,6 +113,7 @@ class View
     <<-DOC
       <meta charset='UTF-8'>
       <meta content='width=device-width, initial-scale=1.0' name='viewport'>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" >
       <meta content='' property='keywords'>
       <meta content='' property='og:title'>
       <meta content='website' property='og:type'>
